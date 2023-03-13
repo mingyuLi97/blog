0c93dd9dc517d1159c0269888008b5296f87efba
mingyuLi97 <mingyuLi97@users.noreply.github.com>
2023-03-13 13:53:56 +0000

    deploy: de792037037ccebe655c650b4648541c775d60cf

---
+++ assets/knowledge_ts_index.md.e665e147.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,o as n,c as a,a as l}from"./app.bc23776b.js";const E=JSON.parse('{"title":"\u7C7B\u578B","description":"","frontmatter":{},"headers":[],"relativePath":"knowledge/ts/index.md"}'),p={name:"knowledge/ts/index.md"},o=l("",10),B=[o];function e(t,c,r,y,F,A){return n(),a("div",null,B)}const D=s(p,[["render",e]]);export{E as __pageData,D as default};
