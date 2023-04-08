5da5d2b0b47d006074cf6972170f3ee2b1919933
limy <mingyuli1997@gmail.com>
2023-04-08 12:00:34 +0800

    feat: 🎸 增加 手写题 nav

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -18,6 +18,10 @@ const config = defineConfig({
       { text: '编程思想', link: '/design/design-pattern/observer' },
       { text: '工程化', link: '/engineer/packages/yalc' },
       { text: '读书笔记', link: '/book-note/vuejs-design/1' },
+      {
+        text: '手写题',
+        link: 'https://mingyuli97.github.io/fe-code-challenges/'
+      },
       // { text: '面试题', link: '/interview/index' },
       { text: '我的掘金', link: 'https://juejin.cn/user/2383396941081934' }
     ],
